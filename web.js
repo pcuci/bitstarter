d42b94c340fd4c9ef29ee15f743dc6a257bc0563
roRisc <paulcu@gmail.com>
2013-07-21 14:10:45 +0000

    adding fs variable

--- web.js
+++ web.js
@@ -2,6 +2,8 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
 app.get('/', function(request, response) {
     var buf = fs.readFileSync('index.html');
     response.send(buf.toString('utf8'));
